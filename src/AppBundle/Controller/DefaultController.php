53e53ad310652730a59425cb582d1ccdb2d4ffce
Licya <licya.puleio@gmail.com>
2015-08-20 22:36:51 +0200

    Fixing views backend
    Fixing views backend

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,64 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
+
+    public function homepageAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        $dailyMenus = $this->getDoctrine()->getRepository('AppBundle:DailyMenu')->findAll();
+
+        return $this->render('default/homepage.html.twig', array(
+                    'daily_menus' => $dailyMenus,
         ));
     }
+
+    public function newspageAction(Request $request)
+    {
+        $news = $this->getDoctrine()->getRepository('AppBundle:News')->findAll();
+
+        return $this->render('default/newspage.html.twig', array(
+                    'news' => $news,
+        ));
+    }
+
+    public function carteAction(Request $request)
+    {
+        $categories = $this->getDoctrine()->getRepository('AppBundle:Category')->findBy(
+        array('enable'=> 1),
+        array('sort' => 'ASC')
+        );
+
+        return $this->render('default/carte.html.twig', array(
+                    'categories' => $categories,
+        ));
+    }
+
+    public function infosPratiquesAction(Request $request)
+    {
+        return $this->render('default/infos-pratiques.html.twig');
+    }
+
+    public function partenairesAction(Request $request)
+    {
+        $partner = $this->getDoctrine()->getRepository('AppBundle:Partner')->findAll();
+
+        return $this->render('default/partenaires.html.twig', array(
+                    'partners' => $partner,
+        ));
+    }
+
+    public function trattoriaAction(Request $request)
+    {
+        return $this->render('default/trattoria.html.twig');
+    }
+
+    public function nousTrouverAction(Request $request)
+    {
+        return $this->render('default/nous-trouver.html.twig');
+    }
+
+    public function nousContacterAction(Request $request)
+    {
+        return $this->render('default/nous-contacter.html.twig');
+    }
+
 }
