e2e5777e10e7259249c0571a74ada0aae5042489
Licya <licya.puleio@gmail.com>
2015-08-20 20:43:44 +0200

    Update fixtures

--- src/AppBundle/DataFixtures/ORM/LoadData.php
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -26,66 +26,88 @@ use Faker\Factory;
 
 class LoadData implements FixtureInterface
 {
-
+    /** @var \Faker\Generator */
+    private $faker;
+    
     /**
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
     {
-        $faker = Factory::create('fr_FR');
+        $this->faker = Factory::create('fr_FR');
+        
         for ($i = 0; $i < 10; $i++) {
-
-            //Propositon Entity
-            $prop = new Proposition();
-            $prop->setTitle($faker->word);           
-            $manager->persist($prop);
-
-            //Category Entity
-            $cat = new Category();
-            $cat->setName($faker->word);
-            $cat->setDescription($faker->sentence());
-            $cat->setSort($i);
-            $cat->setEnable(true);
-            $manager->persist($cat);
-
-            //Product Entity
-            $prod = new Product();
-            $prod->setName($faker->word);
-            $prod->setDescription($faker->sentence());
-            $prod->setMainPrice($faker->randomFloat(10, 100));
-            $prod->setSecondPrice($faker->randomFloat(10, 100));
-            $prod->setEnable(true);
-            $prod->setCategory($cat);
-            $prod->setProposition($prop);
-            $manager->persist($prod);
-
             //News Entity
             $new = new News();
-            $new->setTitle($faker->word);
-            $new->setSubtitle($faker->word);
-            $new->setDescription($faker->sentence());
+            $new->setTitle($this->faker->word);
+            $new->setSubtitle($this->faker->word);
+            $new->setDescription($this->faker->sentence());
             $new->setEnable(true);
             $new->setSort($i);
             $manager->persist($new);
 
             //Partner Entity
             $part = new Partner();
-            $part->setName($faker->word);
-            $part->setWebsiteLink($faker->email);
+            $part->setName($this->faker->word);
+            $part->setWebsiteLink($this->faker->email);
             $part->setEnable(true);
             $part->setSort($i);
             $manager->persist($part);
-
+        }
+        
+        for ($i = 0; $i < rand(2, 4); $i++) {
             //DailyMenu Entity
             $dail = new DailyMenu();
-            $dail->setTitle($faker->word);
-            $dail->setPrice($faker->randomFloat(10, 100));
-            $dail->setDate($faker->dateTime);
+            $dail->setTitle($this->faker->word);
+            $dail->setPrice($this->faker->randomFloat(10, 100));
+            $dail->setDate($this->faker->dateTime);
             $dail->setEnable(true);
-            $dail->addProposition($prop);
             $manager->persist($dail);
+            
+            for ($j = 0; $j < rand(2, 4); $j++) {
+                //Propositon Entity
+                $prop = new Proposition();
+                $prop->setTitle($this->faker->word);           
+                $prop->setDailyMenu($dail);
+                $manager->persist($prop);
+                
+                //Product Entity
+                $prod = $this->createProduct();
+                $prod->setProposition($prop);
+                $manager->persist($prod);
+            }
         }
+        
+        for ($i = 0; $i < rand(2, 4); $i++) {
+            //Category Entity
+            $cat = new Category();
+            $cat->setName($this->faker->word);
+            $cat->setDescription($this->faker->sentence());
+            $cat->setSort($i);
+            $cat->setEnable(true);
+            $manager->persist($cat);
+
+            //Product Entity
+            $prod = $this->createProduct();
+            $prod->setCategory($cat);
+            $manager->persist($prod);
+        }
+        
         $manager->flush();
     }
 
+    /**
+     * @return Product
+     */
+    private function createProduct()
+    {
+        $prod = new Product();
+        $prod->setName($this->faker->word);
+        $prod->setDescription($this->faker->sentence());
+        $prod->setMainPrice($this->faker->randomFloat(10, 100));
+        $prod->setSecondPrice($this->faker->randomFloat(10, 100));
+        $prod->setEnable(true);
+        
+        return $prod;
+    }
 }
